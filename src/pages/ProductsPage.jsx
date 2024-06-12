9374394257f89d1559fbfd29cb841933bec5463a
njoroge james <njorogehjames20@gmail.com>
2024-06-12 20:08:00 +0300

    ft: more!

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -14,7 +14,24 @@ const products = [
     { id: 6, title: 'Product 6', image: 'https://pbs.twimg.com/media/FmlmewCaEAIP9du?format=jpg&name=large' },
     { id: 7, title: 'Product 7', image: 'https://pbs.twimg.com/media/Fmlm2MfacAIBThv?format=jpg&name=large' },
     { id: 8, title: 'Product 8', image: 'https://pbs.twimg.com/media/Fo3m21daQAAyHbb?format=jpg&name=large' },
-
+    { id: 9, title: 'Product 9', image: 'https://pbs.twimg.com/media/Fmlm7gWagAA_7Go?format=jpg&name=large'},
+    { id: 10, title: 'Product 10', image: 'https://pbs.twimg.com/media/FmlnDxTacAIdBWS?format=jpg&name=large'},
+    { id: 11, title: 'Product 11', image: 'https://pbs.twimg.com/media/Fo3nuofakAEoI7I?format=jpg&name=small'},
+    { id: 12, title: 'Product 12', image: 'https://pbs.twimg.com/media/Fo3n2HraQAAzFP9?format=jpg&name=large'},
+    { id: 13, title: 'Product 13', image: 'https://pbs.twimg.com/media/FmPy2pXaEAMY5Gt?format=jpg&name=large'},
+    { id: 14, title: 'Product 14', image: 'https://pbs.twimg.com/media/FmPqSMbakAQbE9C?format=jpg&name=large'},
+    { id: 15, title: 'Product 15', image: 'https://pbs.twimg.com/media/FmPqIrXaYAAfQqw?format=jpg&name=large'},
+    { id: 16, title: 'Product 16', image: 'https://pbs.twimg.com/media/FmPqFXfagAA8kQr?format=jpg&name=large'},
+    { id: 17, title: 'Product 17', image: 'https://pbs.twimg.com/media/FmPp5ubaYAAVmty?format=jpg&name=large'},
+    { id: 18, title: 'Product 18', image: 'https://pbs.twimg.com/media/FmIZWcgaUAc4wxa?format=jpg&name=medium'},
+    { id: 19, title: 'Product 19', image: 'https://pbs.twimg.com/media/FmIZDihagAI8QWU?format=jpg&name=medium'},
+    { id: 20, title: 'Product 20', image: 'https://pbs.twimg.com/media/FmIYZTHaAAEDiXZ?format=jpg&name=medium'},
+    { id: 21, title: 'Product 21', image: 'https://pbs.twimg.com/media/FmIYArgaMAAPG4b?format=jpg&name=medium'},
+    { id: 22, title: 'Product 22', image: 'https://pbs.twimg.com/media/FmBSJA8aUAADbW1?format=jpg&name=large'},
+    { id: 23, title: 'Product 23', image: 'https://pbs.twimg.com/media/FmBSDnKaEAExqXE?format=jpg&name=large'},
+    { id: 24, title: 'Product 24', image: 'https://pbs.twimg.com/media/FmBRbu3acAMZlvD?format=jpg&name=large'},
+    { id: 25, title: 'Product 25', image: 'https://pbs.twimg.com/media/FmBRSGsaYAILaMe?format=jpg&name=large'},
+    
 ];
 
 function ProductsPage() {
